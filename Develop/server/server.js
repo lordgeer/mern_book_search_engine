168797f0c562c01a5538d21883c2b87efadbf37a
Thomas G Musler <thomas.g.musler@gmail.com>
2021-07-09 11:57:47 -0400

    continuing to attempt to solve login errors, server updated with missing const requirements

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -3,12 +3,16 @@ const path = require('path');
 const { ApolloServer } = require('apollo-server-express');
 const db = require('./config/connection');
 const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
+const router = require('express').Router();
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
 	typeDefs,
 	resolvers,
+	context: authMiddleware
 });
 
 server.applyMiddleware({ app });
@@ -22,7 +26,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../client/public/index.html'));
 });
 
 db.once('open', () => {
