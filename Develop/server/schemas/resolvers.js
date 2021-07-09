a25287d7c1d517d285b7dc55bcce7bb0205e460d
Thomas G Musler <thomas.g.musler@gmail.com>
2021-07-09 11:37:34 -0400

    updated api, and app.js for changes elsewhere, updated connection and resolvers for same

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -47,6 +47,32 @@ const resolvers = {
             return {token, user};
 
         },
+        saveBook: async (parent, args, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $addToSet: { savedBooks: args } },
+                { new: true, runValidators: true }
+                );
+
+                return updatedUser;
+            }
+  
+            throw new AuthenticationError('Please Login First!');
+        },
+        removeBook: async (parent, { bookId }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: { bookId } } },
+                    { new: true }
+                );
+
+                return updatedUser;
+            }
+
+            throw new AuthenticationError('Please Login First!');
+          },
     }
 };
 
